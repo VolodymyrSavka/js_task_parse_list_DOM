834042bb327356f2082efb53bc37e126beb489f4
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-09-05 14:30:47 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here
+/* const list = document.getElementsByTagName('li');
+const array = [...list]; */
+
+function getSalary(item) {
+  const salaryString = item.dataset.salary;
+  const cleanedString = salaryString.replace(/[$,]/g, '');
+
+  return Number(cleanedString);
+}
+
+function getEmployees(list) {
+  const employeesArray = [...list];
+  const employeeObjects = employeesArray.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: getSalary(item),
+      age: Number(item.dataset.age),
+    };
+  });
+
+  return employeeObjects;
+}
+
+function sortList(list) {
+  const ar = [...list].sort((a, b) => {
+    return getSalary(b) - getSalary(a);
+  });
+
+  const parentList = list[0].parentNode;
+
+  parentList.innerHTML = '';
+
+  ar.forEach((item) => {
+    parentList.appendChild(item);
+  });
+}
+
+const lists = document.getElementsByTagName('li');
+
+sortList(lists);
+getEmployees(lists);
